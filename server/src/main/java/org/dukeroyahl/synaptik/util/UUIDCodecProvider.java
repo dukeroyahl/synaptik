1bfb954c6c86a5bf8daea6b753fbaef113391ffa
Roudranil Roy <roudranil.roy@icloud.com>
2025-08-10 16:35:50 -0400

    fix: remove MongoConfig class and ensure UUIDCodecProvider is properly annotated for dependency injection

--- server/src/main/java/org/dukeroyahl/synaptik/util/UUIDCodecProvider.java
+++ server/src/main/java/org/dukeroyahl/synaptik/util/UUIDCodecProvider.java
@@ -3,9 +3,11 @@ package org.dukeroyahl.synaptik.util;
 import org.bson.codecs.Codec;
 import org.bson.codecs.configuration.CodecProvider;
 import org.bson.codecs.configuration.CodecRegistry;
+import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.UUID;
 
+@ApplicationScoped
 public class UUIDCodecProvider implements CodecProvider {
 
     @Override
