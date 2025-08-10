04b508baec5ef2b2519c56598c9117f5a2a6c116
Roudranil Roy <roudranil.roy@icloud.com>
2025-08-10 16:55:55 -0400

    fix: enhance UUIDCodec to support decoding from ObjectId and add logging to UUIDCodecProvider

--- server/src/main/java/org/dukeroyahl/synaptik/util/UUIDCodecProvider.java
+++ server/src/main/java/org/dukeroyahl/synaptik/util/UUIDCodecProvider.java
@@ -4,16 +4,26 @@ import org.bson.codecs.Codec;
 import org.bson.codecs.configuration.CodecProvider;
 import org.bson.codecs.configuration.CodecRegistry;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.annotation.PostConstruct;
+import org.jboss.logging.Logger;
 
 import java.util.UUID;
 
 @ApplicationScoped
 public class UUIDCodecProvider implements CodecProvider {
+    
+    private static final Logger logger = Logger.getLogger(UUIDCodecProvider.class);
+    
+    @PostConstruct
+    public void init() {
+        logger.info("UUIDCodecProvider initialized as CDI bean");
+    }
 
     @Override
     @SuppressWarnings("unchecked")
     public <T> Codec<T> get(Class<T> clazz, CodecRegistry registry) {
         if (clazz == UUID.class) {
+            logger.debugf("Providing UUIDCodec for class: %s", clazz.getName());
             return (Codec<T>) new UUIDCodec();
         }
         return null;
